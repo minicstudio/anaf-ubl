60df56b29c0faa8c87eacaa523677d0ea7e3d445
Sandor Lorand <sandor.lorand@minic.ro>
2025-09-25 09:41:06 +0300

    E transport V2 addition

--- src/Transport/V2/Notificare.php
+++ src/Transport/V2/Notificare.php
@@ -2,9 +2,9 @@
 
 namespace MinicStudio\UBL\Transport\V2;
 
-use MinicStudio\UBL\Transport\V1\Correction;
-use MinicStudio\UBL\Transport\V1\NotificareAnterioare;
-use MinicStudio\UBL\Transport\V1\Partner;
+use MinicStudio\UBL\Transport\V2\Correction;
+use MinicStudio\UBL\Transport\V2\NotificareAnterioare;
+use MinicStudio\UBL\Transport\V2\Partner;
 use Sabre\Xml\Writer;
 use Sabre\Xml\XmlSerializable;
 use InvalidArgumentException;
